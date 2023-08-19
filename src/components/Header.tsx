4287f52ea4ec6e1237397cc04e0f988e7b552936
OtabekTohirovich <tohirovo630@gmail.com>
2023-08-19 23:56:51 +0500

    Chages drower menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,16 +11,31 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
 import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import { Stack } from "@mui/material";
-import { Search, Settings } from "@mui/icons-material";
+import { Stack, Typography } from "@mui/material";
+import {
+  MonitorHeart,
+  Search,
+  Settings,
+  Watch,
+  Air,
+  Countertops,
+  Bolt,
+  VolunteerActivism,
+} from "@mui/icons-material";
 
 interface Props {
   window?: () => Window;
 }
 
-const drawerWidth = 240;
-const navItems = ["Home", "About", "Contact"];
+const drawerWidth = 260;
+const navItems = [
+  { icon: <MonitorHeart sx={{color: "#ffda00"}}/>, name: "Salomatlik" },
+  { icon: <Watch sx={{color: "#6ae46a"}}/>, name: "Soatlar" },
+  { icon: <Air sx={{color: "#2d5691"}} />, name: "Atirlar" },
+  { icon: <Countertops sx={{color: "#dd7673"}}/>, name: "Oshxona buyumlari" },
+  { icon: <Bolt sx={{color: "#ffda00"}} />, name: "Eliktronika" },
+  { icon: <VolunteerActivism sx={{color: "#ff0e0d"}} />, name: "Zargarlik buyumlari" },
+];
 
 export default function DrawerAppBar(props: Props) {
   const { window } = props;
@@ -31,73 +46,101 @@ export default function DrawerAppBar(props: Props) {
   };
 
   const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
-      <Typography variant="h6" sx={{ my: 1 }}>
-        MUI
-      </Typography>
+    <Stack onClick={handleDrawerToggle} sx={{ textAlign: "center", height: "100vh", position: 'relative' }}>
+      <img
+        src="https://sharqiy.uz/assets/logo.869dfcda.png"
+        alt="Logo"
+        width={"120px"}
+        height={"50px"}
+        style={{ objectFit: "cover", margin: " 6px 20px" }}
+      />
       <Divider />
       <List>
         {navItems.map((item) => (
-          <ListItem key={item} disablePadding>
-            <ListItemButton sx={{ textAlign: "center" }}>
-              <ListItemText primary={item} />
+          <ListItem key={item.name} disablePadding>
+            <ListItemButton sx={{ textAlign: "start" }}>
+              {item.icon}
+              <ListItemText style={{paddingLeft: "10px"}} primary={item.name} />
             </ListItemButton>
           </ListItem>
         ))}
+        
       </List>
-    </Box>
+      <Typography sx={{position: 'absolute', bottom: 0, left: "15px", fontSize: '12px', fontWeight: "bold"}}>
+      <span style={{color:"#ff0e0d"}}>SHARQIY</span>.<span style={{color: "#6ae46a"}}>UZ</span> huquqlar himoyalangan
+      </Typography>
+    </Stack>
   );
 
   const container =
     window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: "flex", height: "6vh",  }}>
+    <Box sx={{ display: "flex", height: "7.5vh", alignItems: "center" }}>
       <CssBaseline />
       <AppBar
         component="nav"
         sx={{
           position: "relative",
-          width: { xs: "100%", sm: "60%", md: "50%", lg: "40%" },
+          width: { xs: "100%", sm: "60%", md: "50%", lg: "37%", xl: "27%" },
           marginX: "auto",
           bgcolor: "#fff",
           borderBottomRightRadius: "10px",
-          borderBottomLeftRadius: "10px"
+          borderBottomLeftRadius: "10px",
         }}
       >
-        <Toolbar >
+        <Toolbar>
           <IconButton
             color="inherit"
             aria-label="open drawer"
             edge="start"
             onClick={handleDrawerToggle}
-            sx={{ mr: 1, bgcolor: "#f44336" }}
+            sx={{
+              mr: 1,
+              bgcolor: "#f44336",
+              "&:hover": { bgcolor: "#f44336" },
+            }}
             size="small"
           >
             <MenuIcon />
           </IconButton>
           <Stack
-            direction={'row'}
-            sx={{border:" 1px solid #f44336", height: "32px", borderRadius: "15px", overflow: 'hidden', width: {xs: "300px", sm: "600px" } }}
+            direction={"row"}
+            sx={{
+              border: " 1px solid #f44336",
+              height: "32px",
+              borderRadius: "15px",
+              overflow: "hidden",
+              width: { xs: "300px", sm: "600px" },
+              margin: " 0 13px",
+            }}
           >
             <input
               type="text"
-              placeholder="Search..."
-              style={{border: "none", background: "#fff", padding: "0 10px", width: "90%"}}
+              placeholder="Qidiruv..."
+              style={{
+                border: "none",
+                background: "#fff",
+                padding: " 0 10px",
+                width: "90%",
+              }}
             />
-            <IconButton  size="small">
-              <Search  sx={{color: "black"}}/>
+            <IconButton size="small">
+              <Search sx={{ color: "black" }} />
             </IconButton>
           </Stack>
           <IconButton
-            sx={{ bgcolor: "#f44336", marginLeft: '10px', color: "#fff" }}
+            sx={{
+              bgcolor: "#f44336",
+              marginLeft: "10px",
+              color: "#fff",
+              "&:hover": { bgcolor: "#f44336" },
+            }}
             size="small"
-
           >
             <Settings />
           </IconButton>
         </Toolbar>
-        
       </AppBar>
       <Box component="nav">
         <Drawer
